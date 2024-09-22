7b5a7e2ee9ca1f197c26390dde55b1ee5d15bc73
itselcid <its.elcid01@gmail.com>
2024-09-22 20:44:15 +0100

    waa matb9ach t auto edit lheader rah readline darouri lihe mn stdio fo9o

--- include/minishell.h
+++ include/minishell.h
@@ -14,6 +14,7 @@
 # define MINISHELL_H
 
 # include "minishell.h"
+# include <stdio.h>
 # include <dirent.h>
 # include <errno.h>
 # include <fcntl.h>
@@ -23,7 +24,6 @@
 # include <signal.h>
 # include <stdbool.h>
 # include <stdint.h>
-# include <stdio.h>
 # include <stdlib.h>
 # include <sys/types.h>
 # include <sys/wait.h>
